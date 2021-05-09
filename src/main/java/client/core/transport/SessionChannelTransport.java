a615d950beffa7ed0f35c73980b2552cc62320c1
Egor Fedorenko <fedorenko-e@kiszi.ru>
2021-05-09 18:16:44 +0300

    SelectorServer refactoring, SelectorTransport backing up. This update doesnt make a selector type of connection done.

--- src/main/java/client/core/transport/SessionChannelTransport.java
+++ src/main/java/client/core/transport/SessionChannelTransport.java
@@ -8,6 +8,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -18,13 +19,11 @@ public class SessionChannelTransport implements Transport {
     private final int port, timeout;
     private static final char GS = 0x1D;
     private static final char RS = 0x1E;
-    private ByteBuffer rBuffer = ByteBuffer.allocate(100);
-    private ByteBuffer wBuffer = ByteBuffer.allocate(100);
     private String clientName;
     private final Scanner scanner = new Scanner(System.in);
     private MessagingProtocol messagingProtocol;
     private SocketChannel client;
-    private Selector selector;
+    private ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
     public SessionChannelTransport(String host, int port, int timeout) {
         this.host = host;
@@ -34,42 +33,15 @@ public class SessionChannelTransport implements Transport {
 
     @Override
     public void connect() {
-        try {
-            client = SocketChannel.open(new InetSocketAddress(host, port));
-//            client.configureBlocking(false);
-//            selector = Selector.open();
-//            client.register(selector, SelectionKey.OP_CONNECT);
-//            while (true){
-//                selector.select();
-//                Set<SelectionKey> selectionKeys = selector.selectedKeys();
-//                for (SelectionKey key:selectionKeys){
-//                    if(key.isConnectable()){
-//                        SocketChannel client = (SocketChannel) key.channel();
-//                        if (client.isConnectionPending()){
-//                            client.finishConnect();
-//                        }
-//                        client.register(selector,SelectionKey.OP_READ);
-//                    }
-//                    // событие READ - сервер пересылает сообщение
-//                    else if (key.isReadable()) {
-//                        SocketChannel client = (SocketChannel) key.channel();
-//                        // Считываем данные из канала
-//                        //String msg = receive(client);
-//                        //if (msg.isEmpty()){
-//                            // Сервер неисправен, выходим из клиента
-//                           // close(selector);
-//                        }else {
-//                            System.out.println("msg");
-//                        }
-//                    }
-//                }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         log("Type your name");
         clientName = scanner.next();
         messagingProtocol = new MessagingProtocol(clientName);
+        try {
+            client = SocketChannel.open(new InetSocketAddress(host, port));
+            startReceiver();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -84,18 +56,40 @@ public class SessionChannelTransport implements Transport {
     private String tryConverse(String message) throws Exception {
         var buffer = ByteBuffer.wrap((messagingProtocol.messageSelector(message)).getBytes());
         client.write(buffer);
-        buffer = ByteBuffer.allocate(100);
-        var baos = new ByteArrayOutputStream();
-        buffer.clear();
-        client.read(buffer);
-        buffer.flip();
-        while (buffer.hasRemaining()) {
-            baos.write(buffer.get());
-            }
-        //}
-        return new String(baos.toByteArray());
+//        buffer = ByteBuffer.allocate(100);
+//        var baos = new ByteArrayOutputStream();
+//        buffer.clear();
+//        client.read(buffer);
+//        buffer.flip();
+//        while (buffer.hasRemaining()) {
+//            baos.write(buffer.get());
+//        }
+        return "sent";
+        //return new String(baos.toByteArray());
     }
-    private void read(){
+
+    private void startReceiver(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    ByteBuffer buffer = ByteBuffer.allocate(100);
+                    var baos = new ByteArrayOutputStream();
+                    try {
+                        client.read(buffer);
+                        while (buffer.hasRemaining()) {
+                            baos.write(buffer.get());
+                        }
+                        log(baos.toString(StandardCharsets.UTF_8));
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+        thread.start();
 
     }
 
--- src/main/java/server/SessionSelectorServer.java
+++ src/main/java/server/SessionSelectorServer.java
@@ -11,7 +11,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import static util.Logs.log;
@@ -19,43 +19,59 @@ import static util.Logs.log;
 public class SessionSelectorServer implements Runnable {
     private final int timeout;
     private final InetSocketAddress address;
+    Date date = new Date();
+    SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    private final ByteBuffer readBuffer = ByteBuffer.allocate(8);
+    private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    private boolean isMessageReceived = false;
+    private ByteBuffer writeBuffer;
+    private static final char GS = 0x1D;
+    private static final char RS = 0x1E;
+    private Selector selector;
+    private ServerSocketChannel serverChannel;
 
-    private Set<SocketChannel> session;
     public SessionSelectorServer(String host, int port, int timeout) {
-        this.address = new InetSocketAddress(host,port);
-        this.timeout=timeout;
-        this.session = new HashSet<>();
+        this.address = new InetSocketAddress(host, port);
+        this.timeout = timeout;
     }
 
     @Override
     public void run() {
-        try (var serverChannel = ServerSocketChannel.open(); var selector = Selector.open();) {
+        try {
+            serverChannel=ServerSocketChannel.open();
+            selector = Selector.open();
             serverChannel.bind(address);
             serverChannel.configureBlocking(false);
             serverChannel.register(selector, SelectionKey.OP_ACCEPT);
             while (true) {
                 selector.select();
-                var iterator = selector.selectedKeys().iterator();
-                while (iterator.hasNext()) {
-                    var key = iterator.next();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> itr = selectedKeys.iterator();
+                while (itr.hasNext()) {
+                    var key = (SelectionKey) itr.next();
 
                     if (key.isValid() && key.isAcceptable()) {
                         //log("acceptable " + key);
-                        performAccept(key);
+                        //performAccept(key);
+                        var clientSocket = serverChannel.accept();
+                        if (clientSocket!=null){
+                            clientSocket.configureBlocking(false);
+                            clientSocket.register(selector,SelectionKey.OP_READ);
+                            log("accepted " + clientSocket);
+                        }
                     }
                     if (key.isValid() && key.isReadable()) {
                         //log("readable " + key);
-                        ((EchoProtocol) key.attachment()).read();
+                        read(key);
                     }
                     if (key.isValid() && key.isWritable()) {
                         //log("writable " + key);
-                        ((EchoProtocol) key.attachment()).write();
+                        write(key);
                     }
-                    iterator.remove();
+                    itr.remove();
                 }
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -69,18 +85,107 @@ public class SessionSelectorServer implements Runnable {
                 if (clientChannel != null) {
                     clientChannel.configureBlocking(false);
                     var clientKey = clientChannel.register(serverKey.selector(), SelectionKey.OP_READ);
-                    clientKey.attach(new EchoProtocol(clientKey));
+                    //clientKey.attach(new EchoProtocol(clientKey));
                     log("accepted " + clientChannel);
                 }
             }
             while (clientChannel != null);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static class EchoProtocol {
+    public void read(SelectionKey key) {
+        try {
+            tryRead(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            closeConnection(key);
+        }
+    }
+
+    private void tryRead(SelectionKey key) throws Exception {
+        var channel = (SocketChannel) key.channel();
+        int readCount;
+        do {
+            readBuffer.clear();
+            readCount = channel.read(readBuffer);
+            readBuffer.flip();
+            while (readBuffer.hasRemaining() && !isMessageReceived) {
+                var b = readBuffer.get();
+                if (b == RS) {
+                    isMessageReceived = true;
+                } else {
+                    baos.write(b);
+                }
+            }
+        }
+        while (readCount > 0 && !isMessageReceived);
+
+        if (isMessageReceived) {
+            var message = new String(baos.toByteArray());
+            //log("received from " + channel + ": " + message);
+            var parsedMSG = parseMessage(message);
+            writeBuffer = ByteBuffer.wrap((formatter.format(date) + " " + parsedMSG).getBytes()); //+RS
+            //key.interestOpsAnd(~SelectionKey.OP_READ);
+            key.interestOps(SelectionKey.OP_WRITE);
+            //channel.register(key.selector(), SelectionKey.OP_WRITE);
+
+        }
+    }
+
+    private String parseMessage(String msg) {
+        var buff = msg.split(String.valueOf(GS)); //Arrays.toString(<>)
+        if (buff[0].equals("T_REGISTER")) return "New connected user " + buff[1];
+        return buff[1] + " says: " + buff[2];
+    }
+
+    public void write(SelectionKey key) {
+        try {
+            tryWrite();
+        } catch (Exception e) {
+            e.printStackTrace();
+            closeConnection(key);
+        }
+    }
+
+    private void tryWrite() throws Exception {
+        try {
+            selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = keys.iterator();
+
+            while(iter.hasNext()) {
+                SelectionKey key = (SelectionKey) iter.next();
+                if (key.isWritable()) {
+                    SocketChannel clientChannel = (SocketChannel) key.channel();
+                    //ByteBuffer buf = Charset.forName("ISO-8859-2").encode(msg);
+
+                    while(writeBuffer.hasRemaining())
+                        clientChannel.write(writeBuffer);
+
+                    writeBuffer.clear();
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection(SelectionKey key) {
+        key.cancel();
+        if (key.channel() != null) {
+            try {
+                key.channel().close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+    /*private static class EchoProtocol {
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         private final SelectionKey key;
@@ -180,3 +285,4 @@ public class SessionSelectorServer implements Runnable {
         }
     }
 }
+*/
